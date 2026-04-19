db5238175f1d29973a9cb1c47c9e55639e7daeb6
JorgePersonal <jorge.85navarro@gmail.com>
2026-04-19 17:06:49 -0600

    Setup of tests and first test on navbar

---
+++ src/components/Navbar.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {render, screen } from '@testing-library/react';
+import {describe, it, expect} from 'vitest';
+import Navbar from './Navbar';
+import { useAuth } from '../context/AuthContext.jsx';
+
+import { AuthProvider } from '../context/AuthContext.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+
+describe('Navbar component', () => {
+  it('Navbar renders', () => {
+    render(
+        <AuthProvider>
+            <Router>
+                <Navbar />
+            </Router>
+        </AuthProvider>
+    );
+    });
+
+
+    it('Shows correct links when user is not logged in', () => {
+        render(
+            <AuthProvider>
+                <Router>
+                    <Navbar />
+                </Router>
+            </AuthProvider>
+        );
+    expect(screen.getByText('About Rockwell')).toBeInTheDocument();
+    });
+});
